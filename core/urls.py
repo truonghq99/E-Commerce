257dd1c5603fbf319ab5b292d4fa07223161bdf3
NewPTIT <newptit.dev@gmail.com>
2021-06-04 22:49:00 +0700

    update path pages

--- core/urls.py
+++ core/urls.py
@@ -16,6 +16,10 @@ app_name = 'core'
 
 urlpatterns = [
     path('', HomeView.as_view(), name='home'),
+    path('books/', HomeView.as_view(), name='home'),
+    path('electronics/', HomeView.as_view(), name='home'),
+    path('clothes/', HomeView.as_view(), name='home'),
+
     path('checkout/', CheckoutView.as_view(), name='checkout'),
     path('order-summary/', OrderSummaryView.as_view(), name='order-summary'),
     path('product/<slug>/', ItemDetailView.as_view(), name='product'),
